e3a2ac749ced1d96be3edaca850d2a9968c3d3c3
Bailey-Moir <bailey.p.moir@gmail.com>
2025-02-02 21:38:15 +1300

    many backgrounds

--- index.py
+++ index.py
@@ -12,26 +12,64 @@ def bool_input(name: str, default = True) -> bool:
 def float_input(name: str, default = 0.5) -> float:
     raw = input(f'{name} ({default:.1f}f): ')
     try:
-        return math.ceil(10*float(raw))/10
+        # return math.ceil(10*float(raw))/10
+        return float(raw)
+    except ValueError:
+        return default
+
+def int_input(name: str, default = 0) -> int:
+    raw = input(f'{name} ({default:.0f}): ')
+    try:
+        return int(raw)
     except ValueError:
         return default
 
 
 GAP = 10
 
-inner_gap = bool_input('inner gap')
-waybar_gap = bool_input('waybar gap')
-top = bool_input('top', default=False)
-bar_opactiy = float_input('waybar opacity', default=0.5)
-foot_opacity = float_input('foot opacity', default=1.0)
+defaults = []
+with open(os.path.join(os.path.dirname(__file__),"data"), 'r') as source:
+    content = source.read()
 
-# output_path = os.path.expanduser('~/.config/sway/config')
+    defaults = content.split('\n')
 
+is_catpuccin = bool_input('catpuccin', default=(defaults[0] == 'True'))
+background_number = int(defaults[1])
+if is_catpuccin:
+    background_number = int_input('background number', default=background_number)
+inner_gap = bool_input('inner gap', default=(defaults[2] == 'True')) # False
+waybar_gap = bool_input('waybar gap', default=(defaults[3] == 'True'))
+top = bool_input('top', default=(defaults[4] == 'True'))
+corner = int_input('corner radius', default=int(defaults[5]))
+bar_opactiy = float_input('waybar opacity', default=float(defaults[6]))
+foot_opacity = float_input('foot opacity', default=float(defaults[7]))
+
+
+# saving config
+with open(os.path.join(os.path.dirname(__file__),"data"), 'w') as destination:
+    content = ''
+
+    content += str(is_catpuccin)+'\n'
+    content += str(background_number)+'\n'
+    content += str(inner_gap)+'\n'
+    content += str(waybar_gap)+'\n'
+    content += str(top)+'\n'
+    content += str(corner)+'\n'
+    content += str(bar_opactiy)+'\n'
+    content += str(foot_opacity)+'\n'
+
+    destination.write(content)
+
+
+# sway
 with open(os.path.expanduser('~/.config/rice/public/config.sway'), 'r') as source, open(os.path.expanduser('~/.config/sway/config'), 'w') as destination:
     content = source.read()
 
+    backgrounds = os.listdir(os.path.expanduser("~/.config/backgrounds/catpuccin"))
+
     content += f'\ngaps inner {GAP if inner_gap else 0}'
-    # content += f"\nfor_window [app_id=\"foot\"] opacity {foot_opacity}"
+    content += f'\ncorner_radius {corner}'
+    content += f'\noutput * bg ~/.config/backgrounds/{("catpuccin/"+backgrounds[background_number%len(backgrounds)]) if is_catpuccin else "tile.png"} {"fill" if is_catpuccin else "tile"}'
 
     destination.write(content)
 
@@ -45,6 +83,7 @@ with open(os.path.expanduser('~/.config/rice/public/config.waybar'), 'r') as sou
     content = '\n'.join(lines)
     destination.write(content)
 
+# waybar css
 with open(os.path.expanduser('~/.config/rice/public/style.waybar.css'), 'r') as source, open(os.path.expanduser('~/.config/waybar/style.css'), 'w') as destination:
     content = source.read()
 
@@ -58,14 +97,26 @@ with open(os.path.expanduser('~/.config/rice/public/style.waybar.css'), 'r') as 
 
     content += f'window#waybar {{ background: {'transparent' if waybar_gap else '@base'}; }}'
 
-    content = content.replace("@base;", f'@base{f'{100*bar_opactiy:.0f}' if bar_opactiy != 1 else ""};')
+    rounded = 10*math.ceil(10*bar_opactiy)
+    content = content.replace("@base;", f'@base{f'{rounded:.0f}' if rounded != 100 else ""};')
 
     destination.write(content)
 
+# foot
 with open(os.path.expanduser('~/.config/rice/public/config.foot'), 'r') as source, open(os.path.expanduser('~/.config/foot/foot.ini'), 'w') as destination:
     content = source.read()
 
-    content = content.replace("[colors]\n", f'[colors]\nalpha={foot_opacity}\n')
+    content = content.replace('[colors]\n', f'[colors]\nalpha={foot_opacity}\nbackground={"1E1D2D" if is_catpuccin else "161616"}\n')
+
+    destination.write(content)
+
+# theme.css
+with open(os.path.expanduser('~/.config/theme.css'), 'w') as destination:
+    content = ''
+
+    for alpha in range(10,110,10):
+        content += f'@define-color base{alpha if alpha != 100 else ""} rgba({"30, 29, 45" if is_catpuccin else "22, 22, 22"}, {alpha/100});\n'
+        content += f'@define-color white{alpha if alpha != 100 else ""} rgba({"191, 198, 212" if is_catpuccin else "242, 244, 248"}, {alpha/100});\n'
 
     destination.write(content)
 
